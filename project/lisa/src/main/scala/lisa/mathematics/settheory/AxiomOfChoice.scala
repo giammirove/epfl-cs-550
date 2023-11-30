e1921cc5726a955be2a8128bea124cf6885ac75d
Gianmaria Rovelli <rovelli.gianmaria@gmail.com>
2023-11-30 11:57:43 +0100

    [Project] Added image definition

--- project/lisa/src/main/scala/lisa/mathematics/settheory/AxiomOfChoice.scala
+++ project/lisa/src/main/scala/lisa/mathematics/settheory/AxiomOfChoice.scala
@@ -1,7 +1,16 @@
 package lisa.mathematics
 package settheory
 
+import lisa.mathematics.settheory.SetTheory.pair
 import lisa.mathematics.settheory.SetTheory.setIntersection
+import lisa.mathematics.settheory.SetTheory.functional
+import lisa.mathematics.settheory.SetTheory.relationDomain
+import lisa.mathematics.settheory.SetTheory.relationRange
+import lisa.mathematics.settheory.SetTheory.singleton
+import lisa.mathematics.settheory.SetTheory.setIntersection
+import lisa.mathematics.settheory.SetTheory.restrictedFunction
+import lisa.mathematics.settheory.SetTheory.Pi
+import lisa.automation.settheory.SetTheoryTactics.UniqueComprehension
 
 object AxiomOfChoice extends lisa.Main {
   // export everything in this package
@@ -9,10 +18,19 @@ object AxiomOfChoice extends lisa.Main {
   // pairwise_disjoint :: "i ⇒ o"  where
   //   "pairwise_disjoint(A) ≡ ∀A1 ∈ A. ∀A2 ∈ A. A1 ∩ A2 ≠ 0 ⟶ A1=A2"
 
+  private val a = variable
+  private val b = variable
+  private val f = variable
+  private val g = variable
+  private val r = variable
+  private val t = variable
   private val x = variable
   private val y = variable
+  private val z = variable
 
   private val A = variable
+  private val B = variable
+  private val C = variable
 
   val pairwiseDisjoint = DEF(A) --> {
     forall(
@@ -25,4 +43,34 @@ object AxiomOfChoice extends lisa.Main {
     )
   }
 
+  val imageUniqueness = Theorem(
+    ∃!(g, ∀(t, in(t, g) <=> in(t, relationRange(r)) /\ exists(x, in(x, A) /\ in(pair(x, t), r))))
+  ) {
+    have(∃!(g, ∀(t, in(t, g) <=> in(t, relationRange(r)) /\ exists(x, in(x, A) /\ in(pair(x, t), r))))) by UniqueComprehension(
+      relationRange(r),
+      lambda((t, b), exists(x, in(x, A) /\ in(pair(x, t), r)))
+    )
+  }
+
+  // // [[https://isabelle.in.tum.de/dist/library/FOL/ZF/ZF_Base.html#ZF_Base.Image|const]]
+  val image = DEF(r, A) --> The(g, forall(t, in(t, g) <=> in(t, relationRange(r)) /\ exists(x, in(x, A) /\ in(pair(x, t), r))))(imageUniqueness)
+
+  // // [[https://isabelle.in.tum.de/dist/library/FOL/ZF/ZF_Base.html#apply]]
+  // // TODO: not sure about singleton
+  val apply = DEF(f, a) --> union(image(f, a))
+
+  // /*
+  // (* ********************************************************************** *)
+  // (* AC1 ⟹ AC2                                                            *)
+  // (* ********************************************************************** *)
+  //  */
+  // // lemma AC1_AC2_aux1: "⟦f:(∏X ∈ A. X);  B ∈ A;  0∉A⟧ ⟹ {f`B} ⊆ B ∩ {f`C. C ∈ A}"
+  // // by(fast elim !: apply_type)
+  //
+  val AC1_AC2_aux1 = Lemma({
+    val f = Pi(A, A)
+    in(B, A) /\ !in(emptySet, A) |- subset(apply(f, B), setIntersection(B, restrictedFunction(f, A)))
+  }) {
+    sorry
+  }
 }
--- project/lisa/src/main/scala/lisa/settheory/SetTheoryZFCAxioms.scala
+++ project/lisa/src/main/scala/lisa/settheory/SetTheoryZFCAxioms.scala
@@ -3,6 +3,11 @@ package lisa.settheory
 import lisa.fol.FOL.{_, given}
 import lisa.utils.K
 import lisa.utils.K.makeAxiom
+import lisa.mathematics.settheory.AxiomOfChoice.pairwiseDisjoint
+// import lisa.mathematics.settheory.AxiomOfChoice.PiId
+import lisa.mathematics.settheory.SetTheory.setIntersection
+import lisa.mathematics.settheory.SetTheory.singleton
+import lisa.mathematics.settheory.SetTheory.Pi
 
 /**
  * Axioms for the Zermelo-Fraenkel theory (ZFC)
@@ -10,23 +15,53 @@ import lisa.utils.K.makeAxiom
 private[settheory] trait SetTheoryZFCAxioms extends SetTheoryZFAxioms {
   private val x = variable
   private val y = variable
+  private val f = variable
   private val A = variable
   private val B = variable
-  private val P = predicate[3]
-  // private final val sPsi = SchematicPredicateLabel("P", 3)
+  private val C = variable
 
-  /**
-   * Replacement Schema --- If a predicate `P` is 'functional' over `x`, i.e.,
-   * given `a ∈ x`, there is a unique `b` such that `P(x, a, b)`, then the
-   * 'image' of `x` in P exists and is a set. It contains exactly the `b`'s that
-   * satisfy `P` for each `a ∈ x`.
+  final val identiy = ConstantFunctionLabel("identity", 1)
+
+  /*
+   *
+   * Title:      ZF/AC/AC_Equiv.thy
+   *  Author:     Krzysztof Grabczewski
+   *
+   * Axioms AC1 -- AC19 come from "Equivalents of the Axiom of Choice, II"
+   * by H. Rubin and J.E. Rubin, 1985.
+   *
+   * Axiom AC0 comes from "Axiomatic Set Theory" by P. Suppes, 1972.
+   *
+   * Some Isabelle proofs of equivalences of these axioms are formalizations of
+   * proofs presented by the Rubins.  The others are based on the Rubins' proofs,
+   * but slightly changed.
+   *
    */
-  // final val replacementSchema: AXIOM = Axiom(
-  //   "replacementSchema",
-  //   forall(A, in(A, x) ==> existsOne(B, P(x, A, B))) ==>
-  //     exists(y, forall(B, in(B, y) <=> exists(A, in(A, x) /\ P(x, A, B))))
-  // )
 
-  // override def axioms: Set[(String, AXIOM)] = super.axioms + (("replacementSchema", replacementSchema))
+  // "AC1 ≡ ∀A. 0∉A ⟶ (∃f. f ∈ (∏X ∈ A. X))"
+  final val AC1: AXIOM = Axiom(
+    "ac1",
+    // TODO: check Pi(A, A)
+    //       my guess is that since A is like {(x,x), (y,y), ...}, if you
+    //       look at the definition of Pi in Lisa it expands the function argument
+    //       as a relation (set of pairs), but in Isabelle they use
+    //       lambda function to express \x in A. x, that it is the identity
+    //       function restricted to A
+    forall(A, !in(emptySet, A) ==> (exists(f, in(f, Pi(A, A)))))
+  )
+
+  // "AC2 ≡ ∀A. 0∉A ∧ pairwise_disjoint(A)
+  //                  ⟶ (∃C. ∀B ∈ A. ∃y. B ∩ C = {y})"
+  // [[https://isabelle.in.tum.de/dist/library/FOL/ZF-AC/AC_Equiv.html#AC2]]
+  final val AC2: AXIOM = Axiom(
+    "ac2",
+    forall(
+      A,
+      !in(emptySet, A) /\ pairwiseDisjoint(A) ==>
+        exists(C, forall(B, in(B, A) ==> exists(y, setIntersection(B, C) === singleton(y))))
+    )
+  )
+
+  override def axioms: Set[(String, AXIOM)] = super.axioms + (("ac1", AC1)) + (("ac2", AC2))
 
 }
